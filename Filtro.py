5765e91a4a42aaae5b3fce8fe5857da406bb456f
Lariedream <larie.silva1997@gmail.com>
2023-12-04 22:49:22 -0300

    atualizado

--- Filtro.py
+++ Filtro.py
@@ -2,58 +2,105 @@ import os
 import csv
 import re
 import pandas as pd
-
-nome_arquivo = input("Digite o nome inteiro do seu arquivo desejado. Exemplo: Arquivo.csv / Arquivo.xlsx: ")
-
-arquivo_presente = nome_arquivo in os.listdir("dados")
-
-if arquivo_presente:
-    nome, extensao = os.path.splitext(nome_arquivo)
-    extensao = extensao[1:]  # Remover o ponto da extensão
-    if extensao == "csv":    
-        with open(os.path.join("dados", nome_arquivo), newline="") as file:     # Lendo os dados do arquivo
-            reader = csv.DictReader(file)
-            dados = list(reader)
-    elif extensao == "xlsx":
-        df = pd.read_excel(os.path.join("dados", nome_arquivo), engine="openpyxl", dtype={
-    })
-    else:
-        print("extensão de arquivo não encontrado.")
-else:
-    print("O arquivo especificado não está presente no diretório 'dados'.")
-
-idadeMinima= int(input("Digite a idade minima: "))
-
-idadeMaxima = int(input("Digite a idade maxima: "))
-
-taxaMinima = input("Digite a taxa mínima: ")
-taxaMinima = float(re.sub("[^0-9.]", "", taxaMinima).replace(",", "."))  # Convertendo para float
-
-taxaMaxima = input("Digite a taxa máxima: ")
-taxaMaxima = float(re.sub("[^0-9.]", "", taxaMaxima).replace(",", "."))  # Convertendo para float
+from tkinter import *
+from tkinter import messagebox
 
 
-parcelaMinima = input("Digite o valor mínimo da parcela: ")
-parcelaMinima = float(re.sub("[^0-9.]", "", parcelaMinima).replace(",", "."))  # Convertendo para float
+def processar_dados():
+    try:
+        idade_minima = int(entry_idade_minima.get())
+        idade_maxima = int(entry_idade_maxima.get())
 
-parcelaMaxima = input("Digite o valor máximo da parcela: ")
-parcelaMaxima = float(re.sub("[^0-9.]", "", parcelaMaxima).replace(",", "."))  # Convertendo para float
+        taxa_minima = float(re.sub("[^0-9.]", "", entry_taxa_minima.get()).replace(",", "."))
+        taxa_maxima = float(re.sub("[^0-9.]", "", entry_taxa_maxima.get()).replace(",", "."))
 
-if extensao == "csv":
-    filtro = []
-    for linha in dados:
-        if int(linha["Idade"]) >= idadeMinima and int(linha["Idade"]) <= idadeMaxima and float(linha["Taxa"].replace(",", ".")) >= taxaMinima and float(linha["Taxa"].replace(",", ".")) <= taxaMaxima and float(linha["Parcela"].replace(",", ".")) >= parcelaMinima and float(linha["Parcela"].replace(",", ".")) <= parcelaMaxima:
-            filtro.append(linha)
-    with open("Results.csv", "w", newline="") as file: #abrindo o arquivo csv e inserindo uma linha nova no arquivo
-        header= ["Idade", "Taxa", "Parcela"]
-        writer= csv.DictWriter(file, fieldnames = header)
-        writer.writeheader()
-        writer.writerows(filtro)
-elif extensao == "xlsx":
-    filtro = df[
-        (df["Idade"] >= idadeMinima) & (df["Idade"] <= idadeMaxima) &
-        (df["Taxa"] >= taxaMinima) & (df["Taxa"] <= taxaMaxima) &
-        (df["Parcela"].replace(",", ".") >= parcelaMinima) & 
-        (df["Parcela"].replace(",", ".") <= parcelaMaxima)
-    ]  # Salvar os resultados no arquivo Results.csv
-    filtro.to_csv("Results.csv", index=False)
+        parcela_minima = float(re.sub("[^0-9.]", "", entry_parcela_minima.get()).replace(",", "."))
+        parcela_maxima = float(re.sub("[^0-9.]", "", entry_parcela_maxima.get()).replace(",", "."))
+
+        nome_arquivo = entry_nome_arquivo.get()
+
+        arquivo_presente = nome_arquivo in os.listdir("dados")
+
+        if arquivo_presente:
+            nome, extensao = os.path.splitext(nome_arquivo)
+            extensao = extensao[1:]  # Remover o ponto da extensão
+
+            if extensao == "csv":    
+                with open(os.path.join("dados", nome_arquivo), newline="") as file:
+                    reader = csv.DictReader(file)
+                    dados = list(reader)
+            elif extensao == "xlsx":
+                df = pd.read_excel(os.path.join("dados", nome_arquivo), engine="openpyxl", dtype={})
+            else:
+                messagebox.showerror("Erro", "Extensão de arquivo não suportada.")
+                return
+        else:
+            messagebox.showerror("Erro", "O arquivo especificado não está presente no diretório 'dados'.")
+            return
+
+        if extensao == "csv":
+            filtro = []
+            for linha in dados:
+                if (
+                    int(linha["Idade"]) >= idade_minima and int(linha["Idade"]) <= idade_maxima and
+                    float(linha["Taxa"].replace(",", ".")) >= taxa_minima and float(linha["Taxa"].replace(",", ".")) <= taxa_maxima and
+                    float(linha["Parcela"].replace(",", ".")) >= parcela_minima and float(linha["Parcela"].replace(",", ".")) <= parcela_maxima
+                ):
+                    filtro.append(linha)
+            with open("Results.csv", "w", newline="") as file:
+                header = ["Idade", "Taxa", "Parcela"]
+                writer = csv.DictWriter(file, fieldnames=header)
+                writer.writeheader()
+                writer.writerows(filtro)
+        elif extensao == "xlsx":
+            filtro = df[
+                (df["Idade"] >= idade_minima) & (df["Idade"] <= idade_maxima) &
+                (df["Taxa"] >= taxa_minima) & (df["Taxa"] <= taxa_maxima) &
+                (df["Parcela"].replace(",", ".") >= parcela_minima) &
+                (df["Parcela"].replace(",", ".") <= parcela_maxima)
+            ]
+            filtro.to_csv("Results.csv", index=False)
+
+        messagebox.showinfo("Sucesso", "Dados filtrados com sucesso. Resultados salvos em Results.csv.")
+    except ValueError:
+        messagebox.showerror("Erro", "Por favor, insira valores válidos.")
+
+# Crie a janela principal
+janela = Tk()
+janela.title("Filtro de Dados")
+
+# Adicione widgets à janela
+Label(janela, text="Nome do Arquivo:").pack()
+entry_nome_arquivo = Entry(janela)
+entry_nome_arquivo.pack()
+
+Label(janela, text="Idade Mínima:").pack()
+entry_idade_minima = Entry(janela)
+entry_idade_minima.pack()
+
+Label(janela, text="Idade Máxima:").pack()
+entry_idade_maxima = Entry(janela)
+entry_idade_maxima.pack()
+
+Label(janela, text="Taxa Mínima:").pack()
+entry_taxa_minima = Entry(janela)
+entry_taxa_minima.pack()
+
+Label(janela, text="Taxa Máxima:").pack()
+entry_taxa_maxima = Entry(janela)
+entry_taxa_maxima.pack()
+
+Label(janela, text="Parcela Mínima:").pack()
+entry_parcela_minima = Entry(janela)
+entry_parcela_minima.pack()
+
+Label(janela, text="Parcela Máxima:").pack()
+entry_parcela_maxima = Entry(janela)
+entry_parcela_maxima.pack()
+
+# Utilize diretamente a função processar_dados como o comando do botão
+botao_processar = Button(janela, text="Processar Dados", command=processar_dados)
+botao_processar.pack()
+
+# Inicie o loop principal da interface gráfica
+janela.mainloop()
