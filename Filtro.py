03ad33e8992b96758a5136c55945b599355fdace
Lariedream <larie.silva1997@gmail.com>
2024-01-02 20:01:03 -0300

    atualizado

--- Filtro.py
+++ Filtro.py
@@ -15,13 +15,21 @@ def processar_dados():
 
         parcela_minima = float(re.sub("[^0-9.]", "", entry_parcela_minima.get()).replace(",", "."))
         parcela_maxima = float(re.sub("[^0-9.]", "", entry_parcela_maxima.get()).replace(",", "."))
+        
+        
 
         diretorio_selecionado = filedialog.askdirectory()
 
         if not diretorio_selecionado:
             messagebox.showinfo("Aviso", "Nenhum diretório selecionado.")
             return
+        
+        contador_cpf = {}
+        
         resultados = []
+        
+        total_linhas_processadas = 0
+
         for nome_arquivo in os.listdir(diretorio_selecionado):
             arquivo_path = os.path.join(diretorio_selecionado, nome_arquivo)
 
@@ -32,6 +40,9 @@ def processar_dados():
                     with open(arquivo_path, newline="", encoding="utf-8") as file:
                         reader = csv.DictReader(file)
                         for linha in reader:
+                            cpf = linha["CPF"]
+                            total_linhas_processadas += 1
+                            contador_cpf[cpf] = contador_cpf.get(cpf, 0) + 1
                             idade = int(linha["Idade"])
                             taxa = float(linha["Taxa"].replace(",", "."))
                             parcela = float(linha["Parcela"].replace(",", "."))
@@ -40,7 +51,8 @@ def processar_dados():
                             linha_formatada = {
                                 "Idade": idade,
                                 "Taxa": f"{taxa:.2f}",
-                                "Parcela": f"{parcela:.2f}"
+                                "Parcela": f"{parcela:.2f}",
+                                "CPF": cpf
                             }
                             if (
                                 idade >= idade_minima and idade <= idade_maxima and
@@ -48,9 +60,13 @@ def processar_dados():
                                 parcela >= parcela_minima and parcela <= parcela_maxima
                             ):
                                 resultados.append(linha_formatada)
+                                
                 elif extensao == "xlsx":
-                    df = pd.read_excel(arquivo_path, engine="openpyxl", dtype={})
+                    df = pd.read_excel(arquivo_path, engine="openpyxl", dtype={"CPF": str})
                     for _, linha in df.iterrows():
+                        cpf = str(linha["CPF"])
+                        total_linhas_processadas += 1
+                        contador_cpf[cpf] = contador_cpf.get(cpf, 0) + 1
                         idade = int(linha["Idade"])
                         taxa = float(str(linha["Taxa"]).replace(",", "."))
                         parcela = float(str(linha["Parcela"]).replace(",", "."))
@@ -59,7 +75,8 @@ def processar_dados():
                         linha_formatada = {
                             "Idade": idade,
                             "Taxa": f"{taxa:.2f}",
-                            "Parcela": f"{parcela:.2f}"
+                            "Parcela": f"{parcela:.2f}",
+                            "CPF": cpf
                         }
                         if (
                             idade >= idade_minima and idade <= idade_maxima and
@@ -71,10 +88,12 @@ def processar_dados():
             messagebox.showinfo("Aviso", "Nenhum dado encontrado nos arquivos selecionados.")
         else:
             with open("Results.csv", "w", newline="") as file:
-                header = ["Idade", "Taxa", "Parcela"]
+                header = ["CPF","Idade", "Taxa", "Parcela","Linhas"]
                 writer = csv.DictWriter(file, fieldnames=header)
                 writer.writeheader()
-                writer.writerows(resultados)
+                for linha in resultados:
+                    linha["Linhas"] = contador_cpf.get(linha["CPF"], 0)
+                    writer.writerow(linha)
                 messagebox.showinfo("Dados filtrados com sucesso. Resultados salvos em Results.csv.")
     except ValueError:
         messagebox.showerror("Erro", "Por favor, insira valores válidos.")
@@ -82,7 +101,8 @@ def processar_dados():
 janela = Tk()
 janela.title("Filtro de Dados")
 
-# Adicionei widgets à janela
+# Adicionei widgets à 
+
 Label(janela, text="Idade Mínima:").pack()
 entry_idade_minima = Entry(janela)
 entry_idade_minima.pack()
